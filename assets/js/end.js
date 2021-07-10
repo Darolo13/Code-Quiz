87a51b3e584147b6f8372923121f90b4f6529c5e
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-07-10 16:14:25 -0700

    Add end.js variables

---
+++ assets/js/end.js
@@ -0,0 +1,6 @@
+var username = document.querySelector['#username']
+var saveScoreBtn = document.querySelector['#saveScoreBtn']
+var finalScore = document.querySelector['#finalScore']
+var mostRecentScore = document.querySelector['#mostRecentScore']
+
+
